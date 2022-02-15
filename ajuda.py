ee72162f0ad737bfe6bcc888e6a5ccf2e716bf5a
HopeOwl <64351667+HopeOwl@users.noreply.github.com>
2022-02-15 17:18:44 -0300

    Update ajuda.py

--- ajuda.py
+++ ajuda.py
@@ -1,14 +1,19 @@
+#Carregar arquivo Carros.txt
 with open('Carros.txt') as Carros:
     Carros = Carros.readlines()
 
+#Carregar arquivo CarrosXCores.txt
 with open('CarrosXCores.txt') as CarrosXCores:
     CarrosXCores = CarrosXCores.readlines()
 
+#Definir as variaveis de vendas de cada modelo de carro
 vendas_FiatUno = 0
 vendas_ChevroletChevette = 0
 vendas_VWGol = 0
 vendas_VWFusca = 0
 vendas_ChevroletOpala = 0
+
+#Ler a Venda de cada modelo de carro e colocar na variavel criada acima
 for vendas in range(len(CarrosXCores)):
     if CarrosXCores[vendas][0] == '1':
         vendas_FiatUno += 1
@@ -29,6 +34,7 @@ for vendas in range(len(CarrosXCores)):
     if CarrosXCores[vendas][0] == '5':
         vendas_ChevroletOpala += 1
 
+#Imprimir cada variavel na tela
 print(f'Vendas Fiat Uno = {vendas_FiatUno}\n'
       f'Vendas Chevrolet Chevette = {vendas_ChevroletChevette}\n'
       f'Vendas VW Gol = {vendas_VWGol}\n'
